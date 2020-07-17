972cb552720d90437b4de33914c9e63a70c7f8bb
Edvinas <63248412+Edvinas-S@users.noreply.github.com>
2020-07-17 13:44:52 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+![HitCount](http://hits.dwyl.com/edvinas-s/https://edvinas-sgithubio/BMI_calculator/.svg)
+
+# BMI Calculator
+### calculate yours BMI using PHP
+
+This was just for fun && education :)
+
+https://edvinas-s.github.io/BMI_calculator/
+
+#### Author
+[Edvinas](https://github.com/Edvinas-S)
---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>KMI skaiciuokle</title>
+    <link rel="stylesheet" href="./css/main.css">
+</head>
+<body>
+
+    <h1 for="kmi">KMI masės indekso skaičiuoklė:</h1>
+    <div class="content">
+        <p>Į lentelę įveskite duomenis</p>
+        <form action="./php/calculated.php" method="POST" >
+            <label>Jūsų ūgis (metrais, pvz.: 1.68)*:</label><input type="text" name="height"><br>
+            <label>Jūsų svoris (kilogramais, pvz.: 50)*:</label><input type="text" name="weight"><br>
+            <input type="submit" name="submit" value="Skaičiuoti" style="cursor: pointer; margin-left: 150px; margin-top: 10px; color: #FFF; background-color: #137AB2; border: none; border-radius: 5px; padding-top: 5px; padding-bottom: 5px">
+        </form>
+    </div>
+
+</body>
+</html>
